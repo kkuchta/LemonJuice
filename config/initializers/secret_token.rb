7833f2562df0493d4a1fe561967454a7fa8b3c0d
Kevin Kuchta <kevinmkuchta@gmail.com>
2013-05-14 22:14:17 -0700

    Initial revision; fresh rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LemonJuice::Application.config.secret_key_base = '18e003b81fb704d3323f6b7594afa244cc6998e30867e6e189f56638e58e9d6cc519531712db46d7f3fd1df7e4c500b4eba2c804c7e893954818dbd461bbe938'
