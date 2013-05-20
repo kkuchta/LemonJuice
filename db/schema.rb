e6e0f82f4e0daa8fe823c742ecf78f41c1183941
Kevin Kuchta <kevinmkuchta@gmail.com>
2013-05-19 18:29:29 -0700

    Added 'provider_updated_at' column to job listing

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130517063335) do
+ActiveRecord::Schema.define(version: 20130520012825) do
 
   create_table "job_listings", force: true do |t|
     t.string   "title"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20130517063335) do
     t.integer  "job_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "provider_updated_at"
   end
 
   create_table "jobs", force: true do |t|
