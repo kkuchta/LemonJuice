2c06b012a29a3897f10e610d82799747feb59303
Kevin Kuchta <kevinmkuchta@gmail.com>
2013-05-27 00:08:42 -0700

    heroku stuff

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,7 @@ module LemonJuice
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
     config.autoload_paths += Dir["#{config.root}/lib/**/"]
+
+    config.assets.initialize_on_precompile = true
   end
 end
